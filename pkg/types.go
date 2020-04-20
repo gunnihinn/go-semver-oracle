a84366692ffb5a7b9c362398e5405a05864bb9d7
Gunnar Þór Magnússon <gunnar@magnusson.io>
2020-04-20 21:55:07 +0200

    Boilerplate around type comparisons

--- pkg/types.go
+++ pkg/types.go
@@ -1,22 +1,28 @@
 package types
 
 import (
+	"fmt"
 	"go/ast"
+	"reflect"
 )
 
-// Id identifies a definition in a Go package.
+// ID identifies a definition in a Go package.
 // The only things that matter about this type are:
 //
 // - Each declaration in a Go package gets a unique one.
 // - The type can be used as a map key.
-type Id struct {
+type ID struct {
 	name     string
 	receiver string // name of receiver type
 }
 
+func eqID(a, b ID) bool {
+	return a.name == b.name && a.receiver == b.receiver
+}
+
 // Identifiable is an umbrella for different definitions.
 type Identifiable interface {
-	Identify() Id
+	Identify() ID
 }
 
 // Result is the result of comparing identifiable types.
@@ -30,7 +36,61 @@ const (
 
 // Cmp computes the semver difference from a to b.
 func Cmp(a, b Identifiable) Result {
-	// TODO: Use reflect to compare types
+	if !eqID(a.Identify(), b.Identify()) {
+		// TODO: Don't panic?
+		panic("Can't Cmp different a and b")
+	}
+
+	ta := reflect.TypeOf(a)
+	tb := reflect.TypeOf(b)
+
+	// From nothing...
+	if ta == reflect.TypeOf(NoDef{}) {
+		if tb == reflect.TypeOf(NoDef{}) {
+			// ... to nothing
+			// How would we ever end up here?
+			return Equal
+		} else {
+			// ... to something
+			return Minor
+		}
+	}
+
+	if ta != tb {
+		// Type changes are major because Go has no type hierarchy or inference
+		// so, for example, int -> int64 is a major change even though int fits
+		// in int64. In a better type system this might only be a minor change,
+		// but here Go's "volgt rusl" system makes things easy for us.
+		return Major
+	}
+
+	// Same type, not nothing. Have to go type by type.
+	switch ta {
+	case reflect.TypeOf(VarDef{}):
+		x := a.(VarDef)
+		y := b.(VarDef)
+		return cmpVarDef(x, y)
+
+	case reflect.TypeOf(ConstDef{}):
+		x := a.(ConstDef)
+		y := b.(ConstDef)
+		return cmpConstDef(x, y)
+
+	case reflect.TypeOf(TypeDef{}):
+		x := a.(TypeDef)
+		y := b.(TypeDef)
+		return cmpTypeDef(x, y)
+
+	case reflect.TypeOf(FuncDef{}):
+		x := a.(FuncDef)
+		y := b.(FuncDef)
+		return cmpFuncDef(x, y)
+
+	default:
+		// We should have dealt with NoDef already and there aren't other types
+		// that satisfy Identifiable, but just in case.
+		panic(fmt.Sprintf("Unexpected type %s", ta))
+	}
 }
 
 // Identifiable types, one per Go declaration type
@@ -41,11 +101,15 @@ type VarDef struct {
 	spec *ast.ValueSpec
 }
 
-func (def VarDef) Identify() Id {
-	return Id{
-		name:     def.spec.Names[0].Name,
-		receiver: nil,
+func (def VarDef) Identify() ID {
+	return ID{
+		name: def.spec.Names[0].Name,
 	}
+}
+
+func cmpVarDef(a, b VarDef) Result {
+	// TODO
+	return Equal
 }
 
 // ConstDef is the definition of a single constant.
@@ -54,11 +118,15 @@ type ConstDef struct {
 	spec *ast.ValueSpec
 }
 
-func (def ConstDef) Identify() Id {
-	return Id{
-		name:     def.spec.Names[0].Name,
-		receiver: nil,
+func (def ConstDef) Identify() ID {
+	return ID{
+		name: def.spec.Names[0].Name,
 	}
+}
+
+func cmpConstDef(a, b ConstDef) Result {
+	// TODO
+	return Equal
 }
 
 // TypeDef is the definition of a single type.
@@ -67,11 +135,15 @@ type TypeDef struct {
 	spec *ast.TypeSpec
 }
 
-func (def TypeDef) Identify() Id {
-	return Id{
-		name:     def.spec.Name,
-		receiver: nil,
+func (def TypeDef) Identify() ID {
+	return ID{
+		name: def.spec.Name.Name,
 	}
+}
+
+func cmpTypeDef(a, b TypeDef) Result {
+	// TODO
+	return Equal
 }
 
 // FuncDef is the definition of a single function.
@@ -79,18 +151,23 @@ type FuncDef struct {
 	decl *ast.FuncDecl
 }
 
-func (def FuncDef) Identify() Id {
+func (def FuncDef) Identify() ID {
 	var recv string
-	if len(def.Recv.List) > 0 {
-		recv = def.Recv.List[0].Type // TODO
+	if len(def.decl.Recv.List) > 0 {
+		recv = def.decl.Recv.List[0].Type // TODO
 	} else {
-		recv = nil
+		recv = ""
 	}
 
-	return Id{
-		name:     def.decl.Name,
+	return ID{
+		name:     def.decl.Name.Name,
 		receiver: recv,
 	}
+}
+
+func cmpFuncDef(a, b FuncDef) Result {
+	// TODO
+	return Equal
 }
 
 // NoDef is a missing definition.
@@ -98,8 +175,8 @@ type NoDef struct {
 	name string
 }
 
-func (def NoDef) Identify() Id {
-	return Id{
-		name: name,
+func (def NoDef) Identify() ID {
+	return ID{
+		name: def.name,
 	}
 }
