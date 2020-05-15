0ecc74b1dbfd19ff4c742ca98306795328725be5
Gunnar Þór Magnússon <gunnar@magnusson.io>
2020-05-15 09:11:48 +0200

    Draft Parse function

--- pkg/mytypes.go
+++ pkg/mytypes.go
@@ -1,12 +1,20 @@
 package mytypes
 
-import "go/ast"
+import (
+	"fmt"
+	"go/ast"
+)
 
 // Go types
 
 type Comparable interface {
 	compare()
 }
+
+// Null is nothing.
+type Null struct{}
+
+func (n Null) compare() {}
 
 // Primitive is a Go primitive.
 type Primitive int
@@ -47,34 +55,28 @@ func (p Primitive) compare() {}
 // Array is a Go array.
 type Array struct {
 	Values Comparable
-	orig   *ast.Node
+	orig   ast.Node
 }
 
 func (a Array) compare() {}
 
 // Ellipsis is a Go variadic function argument.
-//
-// We need to special-case it because it permits a chain of minor changes that
-// we would otherwise signal as major ones:
-//
-//	func(a int) -> func(as ...int) -> func(as []int)
-//       |      min                min     |
-//       \-------------- maj --------------/
-//
-// Note that compositions of minor changes are not minor, which annoys my inner
-// algebraist.
 type Ellipsis struct {
 	Values Comparable
-	orig   *ast.Node
+	orig   ast.Node
 }
 
 func (e Ellipsis) compare() {}
+
+// TODO: Do we need a Placeholder? It'd be a struct with an ast.Node and a
+// string type name. We'd use it when parsing functions whose arguments are
+// user-defined types we haven't seen yet.
 
 // Map is a Go map.
 type Map struct {
 	Key   Comparable
 	Value Comparable
-	orig  *ast.Node
+	orig  ast.Node
 }
 
 func (m Map) compare() {}
@@ -82,7 +84,7 @@ func (m Map) compare() {}
 // Struct is a Go struct literal.
 type Struct struct {
 	Fields map[string]Comparable
-	orig   *ast.Node
+	orig   ast.Node
 }
 
 func (s Struct) compare() {}
@@ -91,7 +93,7 @@ func (s Struct) compare() {}
 type Lambda struct {
 	Inputs  []Comparable
 	Outputs []Comparable
-	orig    *ast.Node
+	orig    ast.Node
 }
 
 func (l Lambda) compare() {}
@@ -107,7 +109,7 @@ func (i Interface) compare() {}
 type Channel struct {
 	Type Comparable
 	Dir  Direction
-	orig *ast.Node
+	orig ast.Node
 }
 
 func (c Channel) compare() {}
@@ -131,7 +133,7 @@ type Declaration interface {
 type Var struct {
 	Name string
 	Type Comparable
-	orig *ast.Node
+	orig ast.Node
 }
 
 func (v Var) compare() {}
@@ -141,7 +143,7 @@ func (v Var) declare() {}
 type Const struct {
 	Name string
 	Type Comparable
-	orig *ast.Node
+	orig ast.Node
 }
 
 func (c Const) compare() {}
@@ -151,7 +153,7 @@ func (c Const) declare() {}
 type Type struct {
 	Name string
 	Type Comparable
-	orig *ast.Node
+	orig ast.Node
 }
 
 func (t Type) compare() {}
@@ -162,8 +164,149 @@ type Func struct {
 	Name     string
 	Receiver Comparable
 	Lambda
-	orig *ast.Node
+	orig ast.Node
 }
 
 func (f Func) compare() {}
 func (f Func) declare() {}
+
+func Parse(p *ast.Package) ([]Declaration, error) {
+	decls := make([]Declaration, 0)
+
+	for _, file := range p.Files {
+		for _, d := range file.Decls {
+			switch decl := d.(type) {
+			case *ast.GenDecl:
+				for _, s := range decl.Specs {
+					switch spec := s.(type) {
+					case *ast.ValueSpec:
+						t, err := parseExpr(spec.Type)
+						if err != nil {
+							return decls, err
+						}
+
+						for _, name := range spec.Names {
+							decls = append(decls, Var{
+								Name: name.Name,
+								Type: t,
+								orig: name,
+							})
+						}
+
+					case *ast.TypeSpec:
+						t, err := parseExpr(spec.Type)
+						if err != nil {
+							return decls, err
+						}
+
+						decls = append(decls, Type{
+							Name: spec.Name.Name,
+							Type: t,
+							orig: spec,
+						})
+
+					case *ast.ImportSpec:
+						return nil, fmt.Errorf("Unexpected import")
+
+					default:
+						return nil, fmt.Errorf("Unexpected GenDecl type %T", spec)
+					}
+				}
+
+			case *ast.FuncDecl:
+				var r Comparable
+				if decl.Recv == nil {
+					r = Null{}
+				} else {
+					var err error
+					r, err = parseExpr(decl.Recv.List[0].Type)
+					if err != nil {
+						return decls, err
+					}
+				}
+
+				lambda, err := parseLambda(decl.Type)
+				if err != nil {
+					return decls, err
+				}
+
+				decls = append(decls, Func{
+					Name:     decl.Name.Name,
+					Receiver: r,
+					orig:     decl,
+					Lambda:   lambda,
+				})
+
+			case *ast.BadDecl:
+				// TODO
+				panic("Not implemented")
+
+			default:
+				return nil, fmt.Errorf("Unexpected declaration type %T", decl)
+			}
+		}
+	}
+
+	return decls, nil
+}
+
+func parseExpr(e ast.Expr) (Comparable, error) {
+	/*
+		ast.Expr nodes can be:
+
+		BadExpr
+		Ident
+		Ellipsis
+		BasicLit
+		FuncLit
+		CompositeLit
+		ParenExpr
+		SelectorExpr
+		IndexExpr
+		SliceExpr
+		TypeAssertExpr
+		CallExpr
+		StarExpr
+		UnaryExpr
+		BinaryExpr
+		KeyValueExpr
+
+		ArrayType
+		StructType
+		FuncType
+		InterfaceType
+		MapType
+		ChanType
+
+		Not all of those can appear in variable definitions.
+	*/
+
+	switch te := e.(type) {
+	case *ast.Ident:
+
+	case *ast.BasicLit:
+
+	case *ast.FuncLit:
+
+	case *ast.ArrayType:
+
+	case *ast.StructType:
+
+	case *ast.FuncType:
+
+	case *ast.InterfaceType:
+
+	case *ast.MapType:
+
+	case *ast.ChanType:
+
+	default:
+		return Null{}, fmt.Errorf("Unexpected type %s", te)
+	}
+
+	return Null{}, nil
+}
+
+func parseLambda(e ast.Expr) (Lambda, error) {
+	return Lambda{}, nil
+}
